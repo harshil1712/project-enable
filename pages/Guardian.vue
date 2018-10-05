9a77fe9b68f22fc7c905178789753d5cf3f8ef85
San97 <snehdesai97@gmail.com>
2018-10-06 05:28:30 +0530

    Guardian Signup final

--- pages/Guardian.vue
+++ pages/Guardian.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-form v-model="valid">
+  <v-form ref="form" v-model="valid" lazy-validation>
     <v-text-field
       v-model="name"
       :rules="nameRules"
@@ -7,6 +7,7 @@
       label="Name"
       required
     ></v-text-field>
+
      <v-text-field
       v-model="name"
       :rules="nameRules"
@@ -14,6 +15,7 @@
       label="Child Name"
       required
     ></v-text-field>
+
      <v-text-field
       v-model="name"
       :rules="nameRules"
@@ -29,6 +31,7 @@
       label="Address"
       required
     ></v-text-field>
+
      <v-text-field
       v-model="name"
       :rules="nameRules"
@@ -36,6 +39,7 @@
       label="Learning Hours"
       required
     ></v-text-field>
+
      <v-text-field
       v-model="name"
       :rules="nameRules"
@@ -51,6 +55,53 @@
       required
     ></v-text-field>
     
-  </v-form>
+    
+    
+ 
+  <v-container grid-list-md>
+    <v-layout row wrap>
+      <v-flex xs12 lg6>
+        <v-menu
+          ref="menu1"
+          :close-on-content-click="false"
+          v-model="menu1"
+          :nudge-right="40"
+          lazy
+          transition="scale-transition"
+          offset-y
+          full-width
+          max-width="290px"
+          min-width="290px"
+        >
+          <v-text-field
+            slot="activator"
+            v-model="dateFormatted"
+            label="Date of child"
+            hint="MM/DD/YYYY format"
+            persistent-hint
+            prepend-icon="event"
+            @blur="date = parseDate(dateFormatted)"
+          ></v-text-field>
+          <v-date-picker v-model="date" no-title @input="menu1 = false"></v-date-picker>
+        </v-menu>
+        <p>Date in ISO format: <strong>{{ date }}</strong></p>
+      </v-flex>
+      </v-layout>
+  </v-container>
+  <v-checkbox
+      v-model="checkbox"
+      :rules="[v => !!v || 'You must agree to continue!']"
+      label="Do you agree?"
+      required
+    ></v-checkbox>
+
+    <v-btn
+      :disabled="!valid"
+      @click="submit"
+    >
+      submit
+    </v-btn>
+    <v-btn @click="clear">clear</v-btn>
+   </v-form>
    
 </template>
