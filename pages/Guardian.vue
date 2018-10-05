2ffd2158a7a66777130eca33ac864962ff104343
San97 <snehdesai97@gmail.com>
2018-10-06 04:44:57 +0530

    Guardian Signup 1

---
+++ pages/Guardian.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-form v-model="valid">
+    <v-text-field
+      v-model="name"
+      :rules="nameRules"
+      :counter="20"
+      label="Name"
+      required
+    ></v-text-field>
+     <v-text-field
+      v-model="name"
+      :rules="nameRules"
+      :counter="20"
+      label="Child Name"
+      required
+    ></v-text-field>
+     <v-text-field
+      v-model="name"
+      :rules="nameRules"
+      :counter="20"
+      label="Disability"
+      required
+    ></v-text-field>
+     
+     <v-text-field
+      v-model="name"
+      :rules="nameRules"
+      :counter="20"
+      label="Address"
+      required
+    ></v-text-field>
+     <v-text-field
+      v-model="name"
+      :rules="nameRules"
+      :counter="20"
+      label="Learning Hours"
+      required
+    ></v-text-field>
+     <v-text-field
+      v-model="name"
+      :rules="nameRules"
+      :counter="20"
+      label="Time Preference"
+      required
+    ></v-text-field>
+    
+    <v-text-field
+      v-model="email"
+      :rules="emailRules"
+      label="E-mail"
+      required
+    ></v-text-field>
+    
+  </v-form>
+   
+</template>
