273c873030e2af209d727dc335a791f6617daeb9
Het Patel <coder4199@gmail.com>
2018-10-06 13:27:49 +0530

    video chat

---
+++ pages/studentdashboard.vue
@@ -0,0 +1,9 @@
+<template>
+<v-layout>
+ <nuxt-link to='inspire'>
+    <v-card-actions>
+        <v-btn flat color="white"><h1>Start Session</h1></v-btn>
+    </v-card-actions>
+ </nuxt-link>
+</v-layout>
+</template> 
