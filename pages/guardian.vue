aaa1aa992053c5c85c75aa57bbb3de7e59014e05
San97 <31317295+San97@users.noreply.github.com>
2018-10-06 11:09:52 +0530

    Guardian signup (#6)
    * Guardian Signup 1
    
    * Guardian Signup 1
    
    * Guardian Signup final
    
    * Guardian Signup final
    
    * Guardian&Tutor Signup final
    
    * guardian and tutor login page

---
+++ pages/guardian.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-app id="inspire">
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Guardian SIGN UP FORM</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-tooltip bottom>
+                  <v-btn
+                    slot="activator"
+                    :href="source"
+                    icon
+                    large
+                    target="_blank"
+                  >
+
+                  </v-btn>
+                  <span>Source</span>
+                </v-tooltip>
+                <v-tooltip right>
+                  <v-btn slot="activator" icon large href="https://codepen.io/johnjleider/pen/wyYVVj" target="_blank">
+                    <v-icon large>mdi-codepen</v-icon>
+                  </v-btn>
+                  <span>Codepen</span>
+                </v-tooltip>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field prepend-icon="person" name="FULL NAME" label="FULL NAME" type="text"></v-text-field>
+                  <v-text-field prepend-icon="person" name="Child's Name" label="Child's Name" type="text"></v-text-field>
+                  <v-text-field prepend-icon="person" name="Disability" label="Disability" type="text"></v-text-field>
+                  <v-text-field prepend-icon="person" name="Address" label="Address" type="text"></v-text-field>
+                  <v-text-field prepend-icon="person" name="E-MAIL ID" label="E-MAIL ID" type="text"></v-text-field>
+                  <v-text-field prepend-icon="person" name="Learning Hours" label="Learning Hours" type="text"></v-text-field>
+                  <v-text-field prepend-icon="person" name="Time Preference" label="Time Preference" type="text"></v-text-field>
+                  <v-text-field prepend-icon="person" name="Age" label="Age" type="text"></v-text-field>
+                  
+                  <v-text-field id="password" prepend-icon="lock" name="password" label="Password" type="password"></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary">SUBMIT</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: null
+    }),
+    props: {
+      source: String
+    }
+  }
+</script>
